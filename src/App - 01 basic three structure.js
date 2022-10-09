1aa0eeaac2a6dae87c6ef44dc87094d65dd6e1e1
francoisbonnard <francois.bonnard@gmail.com>
2022-10-09 19:12:34 +0200

    Frist

---
+++ src/App - 01 basic three structure.js
@@ -0,0 +1,39 @@
+import "./App.css"
+import * as THREE from "three"
+
+function App() {
+  const scene = new THREE.Scene()
+  const camera = new THREE.PerspectiveCamera(
+    75,
+    window.innerWidth / window.innerHeight,
+    0.1,
+    1000
+  )
+  const renderer = new THREE.WebGLRenderer()
+  renderer.setSize(window.innerWidth, window.innerHeight)
+  document.body.innerHTML = ""
+  document.body.appendChild(renderer.domElement)
+  const geometry = new THREE.BoxGeometry()
+  const material = new THREE.MeshBasicMaterial({
+    color: "blue",
+  })
+  camera.position.z = 5
+  const cube = new THREE.Mesh(geometry, material)
+  scene.add(cube)
+
+  function animate() {
+    requestAnimationFrame(animate)
+    renderer.render(scene, camera)
+  }
+
+  animate()
+  window.addEventListener("resize", () => {
+    renderer.setSize(window.innerWidth, window.innerHeight)
+    camera.aspect = window.innerWidth / window.innerHeight
+    camera.updateProjectionMatrix()
+  })
+
+  return null
+}
+
+export default App
---
+++ src/App.js
@@ -0,0 +1,52 @@
+import "./App.css"
+import { Canvas, useFrame, useThree, extend } from "react-three-fiber"
+import { useRef, useEffect } from "react"
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls"
+extend({ OrbitControls })
+
+const Orbit = () => {
+  const { camera, gl } = useThree()
+  useEffect(() => {
+    const controls = new OrbitControls(camera, gl.domElement)
+    controls.minDistance = 3
+    controls.maxDistance = 20
+    return () => {
+      controls.dispose()
+    }
+  }, [camera, gl])
+  return null
+}
+
+const Box = (props) => {
+  const ref = useRef()
+  useFrame((state) => {
+    ref.current.rotation.x += 0.01
+    ref.current.rotation.y += 0.01
+  })
+  return (
+    <mesh
+      ref={ref}
+      {...props}
+    >
+      <boxBufferGeometry />
+      <meshBasicMaterial color='blue' />
+    </mesh>
+  )
+}
+
+function App() {
+  return (
+    <div style={{ height: "100vh", width: "100vw" }}>
+      <Canvas
+        style={{ backgroundColor: "darkgrey" }}
+        camera={{ position: [3, 3, 3] }}
+      >
+        <Box position={[1, 1, 0]} />
+        <Orbit />
+        <axesHelper args={[5]} />
+      </Canvas>
+    </div>
+  )
+}
+
+export default App
